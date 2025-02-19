75fdfb3daa8da2cdbf0984bbeb0094ed19541c04
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-02-19 15:59:04 +0900

    Add: 프로젝트 생성

---
+++ skin-api/src/main/java/com/personal/skin_api/SkinApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.skin_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinApiApplication.class, args);
+	}
+
+}
---
+++ skin-api/src/test/java/com/personal/skin_api/SkinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.skin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
