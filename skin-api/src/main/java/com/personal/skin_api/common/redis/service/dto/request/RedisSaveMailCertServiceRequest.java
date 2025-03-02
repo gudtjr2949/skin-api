9de86638afdf8963b69899be7e075a793ae9fe55
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-03-02 18:11:53 +0900

    Feat: email, memberName String -> 객체로 변경

--- skin-api/src/main/java/com/personal/skin_api/common/redis/service/dto/request/RedisSaveMailCertServiceRequest.java
+++ skin-api/src/main/java/com/personal/skin_api/common/redis/service/dto/request/RedisSaveMailCertServiceRequest.java
@@ -1,19 +1,24 @@
 package com.personal.skin_api.common.redis.service.dto.request;
 
 import com.personal.skin_api.mail.service.MailPurpose;
+import com.personal.skin_api.member.repository.entity.email.Email;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class RedisSaveMailCertServiceRequest {
     private final MailPurpose purpose;
-    private final String email;
+    private final Email email;
     private final String code;
 
     @Builder
     private RedisSaveMailCertServiceRequest(final MailPurpose purpose, final String email, final String code) {
         this.purpose = purpose;
-        this.email = email;
+        this.email = new Email(email);
         this.code = code;
+    }
+
+    public String getEmail() {
+        return email.getEmail();
     }
 }
