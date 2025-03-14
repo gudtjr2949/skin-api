583e9c81b4383e563d7f5ac25d41c8460c5c4a11
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-03-14 20:03:16 +0900

    Feat: ProductService 제품 수정 기능 추가

--- skin-api/src/main/java/com/personal/skin_api/product/service/ProductService.java
+++ skin-api/src/main/java/com/personal/skin_api/product/service/ProductService.java
@@ -2,6 +2,7 @@ package com.personal.skin_api.product.service;
 
 import com.personal.skin_api.product.service.dto.request.ProductFindListServiceRequest;
 import com.personal.skin_api.product.service.dto.request.ProductFindMyListServiceRequest;
+import com.personal.skin_api.product.service.dto.request.ProductModifyServiceRequest;
 import com.personal.skin_api.product.service.dto.request.ProductRegisterServiceRequest;
 import com.personal.skin_api.product.service.dto.response.ProductDetailResponse;
 import com.personal.skin_api.product.service.dto.response.ProductListResponse;
@@ -12,4 +13,5 @@ public interface ProductService {
     ProductListResponse findProducts(ProductFindListServiceRequest request);
     ProductListResponse findMyProducts(ProductFindMyListServiceRequest request);
     ProductDetailResponse findProductDetail(Long productId);
+    void modifyProduct(ProductModifyServiceRequest request);
 }
---
+++ skin-api/src/main/java/com/personal/skin_api/product/service/dto/request/ProductModifyServiceRequest.java
@@ -0,0 +1,30 @@
+package com.personal.skin_api.product.service.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+public class ProductModifyServiceRequest {
+    private final Long productId;
+    private final String email;
+    private final String newProductName;
+    private final String newProductContent;
+    private final int newPrice;
+    private final MultipartFile newFile;
+
+    @Builder
+    private ProductModifyServiceRequest(final Long productId,
+                                        final String email,
+                                        final String newProductName,
+                                        final String newProductContent,
+                                        final int newPrice,
+                                        final MultipartFile newFile) {
+        this.productId = productId;
+        this.email = email;
+        this.newProductName = newProductName;
+        this.newProductContent = newProductContent;
+        this.newPrice = newPrice;
+        this.newFile = newFile;
+    }
+}
