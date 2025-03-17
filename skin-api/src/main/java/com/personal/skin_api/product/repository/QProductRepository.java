eaeefbe5d950f1b0ab38dcae6968b3383ce38c32
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-03-17 19:10:14 +0900

    Feat: QProductRepository 정렬 알고리즘 추가

--- skin-api/src/main/java/com/personal/skin_api/product/repository/QProductRepository.java
+++ skin-api/src/main/java/com/personal/skin_api/product/repository/QProductRepository.java
@@ -3,12 +3,12 @@ package com.personal.skin_api.product.repository;
 import com.personal.skin_api.member.repository.entity.Member;
 import com.personal.skin_api.product.repository.entity.Product;
 import com.personal.skin_api.product.repository.entity.QProduct;
+import com.personal.skin_api.product.service.dto.ProductSorter;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,33 +17,20 @@ import java.util.List;
 public class QProductRepository {
 
     private final EntityManager em;
-
     private final JPAQueryFactory queryFactory;
+    public static final int PRODUCTS_PAGE_SIZE = 5;
 
     public QProductRepository(EntityManager em) {
         this.em = em;
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public static final int PRODUCTS_PAGE_SIZE = 5;
-
     public List<Product> findProducts(Long productId, String sorter, String keyword) {
         BooleanBuilder builder = new BooleanBuilder();
-        OrderSpecifier<?> orderSpecifier = QProduct.product.id.desc();
+        OrderSpecifier<?> orderSpecifier = ProductSorter.getOrderSpecifier(sorter);
 
         if (productId > 0) {
             builder.and(QProduct.product.id.lt(productId));
-        }
-
-        if (sorter != null) {
-            switch (sorter) {
-                case "views":
-                    orderSpecifier = QProduct.product.productViews.productViews.desc(); // 조회수 내림차순 정렬
-                    break;
-                case "price":
-                    orderSpecifier = QProduct.product.price.price.asc();
-                    break;
-            }
         }
 
         if (keyword != null) {
