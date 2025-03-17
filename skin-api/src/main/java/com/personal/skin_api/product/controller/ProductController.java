8d5abe6570ddea6d66b74bfe722adb3684c39958
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-03-17 18:56:37 +0900

    Feat: ProductController 제품 수정 기능 추가

--- skin-api/src/main/java/com/personal/skin_api/product/controller/ProductController.java
+++ skin-api/src/main/java/com/personal/skin_api/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.personal.skin_api.product.controller;
 
 import com.personal.skin_api.common.dto.CommonResponse;
+import com.personal.skin_api.product.controller.dto.request.ProductModifyRequest;
 import com.personal.skin_api.product.controller.dto.request.ProductRegisterRequest;
 import com.personal.skin_api.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -27,5 +28,13 @@ public class ProductController {
         productService.registerProduct(request.toService(userDetails.getUsername()));
 
         return ResponseEntity.ok().body(new CommonResponse(HttpStatus.OK.value(), "제품 등록 완료"));
+    }
+
+    @PostMapping("/modify")
+    public ResponseEntity<CommonResponse> modifyProduct(@ModelAttribute ProductModifyRequest request,
+                                                @AuthenticationPrincipal UserDetails userDetails) {
+        productService.modifyProduct(request.toService(userDetails.getUsername()));
+
+        return ResponseEntity.ok().body(new CommonResponse(HttpStatus.OK.value(), "제품 수정 완료"));
     }
 }
