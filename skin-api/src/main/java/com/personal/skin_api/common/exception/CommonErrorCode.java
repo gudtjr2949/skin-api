767ac066ef8a459a6e98bd471bdfe10eb10aa290
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-03-17 19:55:17 +0900

    Feat: 잘못된 파라미터 요청 예외 처리 기능 추가

--- skin-api/src/main/java/com/personal/skin_api/common/exception/CommonErrorCode.java
+++ skin-api/src/main/java/com/personal/skin_api/common/exception/CommonErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "입력된 파라미터 값이 올바르지 않습니다."),
+    PARAMETER_CAN_NOT_BE_NULL(HttpStatus.BAD_REQUEST, "입력된 파라미터가 null일 수 없습니다."),
+    INVALID_HTTP_METHOD(HttpStatus.BAD_REQUEST, "잘못된 HTTP Method 요청입니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "리소스를 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생했습니다."),
     ;
