ce628d86275e23fdb624774115c982554d0bb989
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-03-26 15:11:20 +0900

    Feat: OrderRepository findMyOrders 메서드 삭제

--- skin-api/src/main/java/com/personal/skin_api/order/repository/OrderRepository.java
+++ skin-api/src/main/java/com/personal/skin_api/order/repository/OrderRepository.java
@@ -1,13 +1,10 @@
 package com.personal.skin_api.order.repository;
 
-import com.personal.skin_api.member.repository.entity.Member;
 import com.personal.skin_api.order.repository.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findOrdersByMember(Member member);
+
 }
