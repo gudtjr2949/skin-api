a444ce1d7e4068dcc47cebd33a25de3ed04c222b
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-04-01 17:26:37 +0900

    Feat: PaymentController 결제 정보 생성 기능 추가

---
+++ skin-api/src/main/java/com/personal/skin_api/payment/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.personal.skin_api.payment.controller;
+
+import com.personal.skin_api.common.dto.CommonResponse;
+import com.personal.skin_api.payment.controller.dto.request.PaymentCreateRequest;
+import com.personal.skin_api.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/payments")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createPayment(@RequestBody PaymentCreateRequest request,
+                                                @AuthenticationPrincipal UserDetails userDetails) {
+        paymentService.createPayment(request.toService(userDetails.getUsername()));
+        return ResponseEntity.ok().body(new CommonResponse(200, "결제 정보 저장 성공"));
+    }
+}
