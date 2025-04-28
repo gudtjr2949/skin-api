35fa0dcfcb04cae6240f471bc06c9da8251f858a
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-04-28 16:31:44 +0900

    Feat: Preflight CORS 추가

--- skin-api/src/main/java/com/personal/skin_api/common/security/config/WebSecurityConfig.java
+++ skin-api/src/main/java/com/personal/skin_api/common/security/config/WebSecurityConfig.java
@@ -2,10 +2,12 @@ package com.personal.skin_api.common.security.config;
 
 import com.personal.skin_api.common.security.JwtFilter;
 
+import com.personal.skin_api.member.repository.entity.MemberRole;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,7 +22,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
-
 
 @Configuration
 @EnableWebSecurity
@@ -39,7 +40,15 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(cors -> cors.configurationSource(corsConfigurationSource())); // ✅ CORS 설정 추가
+
         http.exceptionHandling(e -> e.authenticationEntryPoint(jwtAuthenticationEntryPoint).accessDeniedHandler(jwtAccessDeniedHandler));
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()  // ✅ Preflight 요청 허용
+                        .requestMatchers("/api/v1/**").hasAuthority(MemberRole.GENERAL.toString()) // ✅ /api/v1/** 는 USER 권한 필요
+                        .anyRequest().permitAll() // 나머지는 모두 허용
+                );
 
         // 권한 규칙 설정
 //        http.logout((logoutConfig) -> {
@@ -63,7 +72,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://220.122.59.199:3000")); // 허용할 프론트엔드 도메인
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 허용할 프론트엔드 도메인
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "WS")); // 허용할 HTTP 메서드
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cookie")); // 허용할 헤더
         configuration.setAllowCredentials(true); // ✅ 쿠키 허용 (HttpOnly 토큰 사용 시 필요)
