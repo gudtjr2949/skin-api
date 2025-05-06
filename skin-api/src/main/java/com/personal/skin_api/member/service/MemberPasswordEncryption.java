a076a55ad4328b402dd2fdefbf117a2057170be6
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-05-06 17:54:31 +0900

    Feat: 회원 비밀번호 암호화, 복호화 메서드 구현

---
+++ skin-api/src/main/java/com/personal/skin_api/member/service/MemberPasswordEncryption.java
@@ -0,0 +1,27 @@
+package com.personal.skin_api.member.service;
+
+import com.personal.skin_api.common.exception.RestApiException;
+import com.personal.skin_api.common.exception.member.MemberErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberPasswordEncryption {
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    public String encodePassword(final String rawPassword) {
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        return encodedPassword;
+    }
+
+    public void comparePassword(final String rawPassword,
+                                final String encodedPassword) {
+        boolean result = passwordEncoder.matches(rawPassword, encodedPassword);
+
+        if (!result) throw new RestApiException(MemberErrorCode.MEMBER_NOT_FOUND);
+    }
+}
